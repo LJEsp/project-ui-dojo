fab155395bf14941e5f3cad43b589bd1c7003941
LJEsp <esparragoleandroj@gmail.com>
2019-04-23 21:31:42 +0800

    Add initial files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,26 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import styled from "styled-components";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/seo";
+import Layout from "../components/layout";
+import Image from "../components/image";
+
+const ImgWrapper = styled.div`
+  max-width: 300px;
+  margin-bottom: 1.45rem;
+`;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <ImgWrapper>
       <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    </ImgWrapper>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
